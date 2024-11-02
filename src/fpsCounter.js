4aec633b9cb1b81d616cdcdadcedb8093927cf13
Mihó Dániel <miho.daniel@mdy.hu>
2024-11-02 21:34:17 +0100

    add more fps feedback

--- src/fpsCounter.js
+++ src/fpsCounter.js
@@ -1,9 +1,24 @@
 import { Text } from "pixi.js";
 
+const FPS_KEEP = 60;
+
 export function createFpsCounter(app) {
+  let arrFps = [];
+  let minFps = Infinity;
+  let maxFps = 0;
+
   // Create an FPS Counter
-  const fpsCounter = new Text({
-    text: "FPS: 0",
+  const avgFpsCounter = new Text({
+    text: "FPS: 999",
+    style: {
+      fontFamily: "Arial",
+      fontSize: 12,
+      fill: 0x000000,
+      align: "center",
+    },
+  });
+  const minFpsCounter = new Text({
+    text: "Min FPS: 999",
     style: {
       fontFamily: "Arial",
       fontSize: 12,
@@ -12,11 +27,41 @@ export function createFpsCounter(app) {
     },
   });
 
-  fpsCounter.position.set(app.screen.width / 2, 10);
-
-  app.ticker.add(() => {
-    fpsCounter.text = `FPS: ${Math.floor(app.ticker.FPS)}`;
+  const maxFpsCounter = new Text({
+    text: "Max FPS: 999",
+    style: {
+      fontFamily: "Arial",
+      fontSize: 12,
+      fill: 0x000000,
+      align: "center",
+    },
   });
 
-  app.stage.addChild(fpsCounter);
+  app.stage.addChild(avgFpsCounter);
+  app.stage.addChild(minFpsCounter);
+  app.stage.addChild(maxFpsCounter);
+
+  avgFpsCounter.position.set(10, 10);
+  minFpsCounter.position.set(avgFpsCounter.width + 20, 10);
+  maxFpsCounter.position.set(
+    avgFpsCounter.width + minFpsCounter.width + 30,
+    10
+  );
+
+  app.ticker.add(() => {
+    // Update min and max FPS
+    minFps = Math.min(app.ticker.FPS, minFps);
+    maxFps = Math.max(app.ticker.FPS, maxFps);
+
+    // Update average FPS
+    arrFps.push(app.ticker.FPS);
+    if (arrFps.length > FPS_KEEP) {
+      arrFps.shift();
+    }
+
+    const averageFps = arrFps.reduce((a, b) => a + b) / arrFps.length;
+    avgFpsCounter.text = `FPS: ${Math.floor(averageFps)}`;
+    minFpsCounter.text = `Min FPS: ${Math.floor(minFps)}`;
+    maxFpsCounter.text = `Max FPS: ${Math.floor(maxFps)}`;
+  });
 }
