66caa4942bd59ae4cb64980b81a32ac2ae114d18
Mihó Dániel <miho.daniel@mdy.hu>
2024-11-02 21:27:26 +0100

    adjust richness to be more realistic

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const GRID_ROWS = 21;
 export const GRID_COLS = 40;
 export const GRID_GAP = 10;
 export const GRID_CELL_SIZE = 24;
-export const GRID_RICHNESS = 1;
+export const GRID_RICHNESS = 0.2;
 
 export const ELEMENT_RADIUS = 8;
 export const ELEMENTS = {
